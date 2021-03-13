22392d25db0aaca0b74a50e73736bcc9149f9d13
Henadzi <RudchenkoHenadzi@users.noreply.github.com>
2021-03-13 23:55:20 +0300

    feat: result task: Towel Sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,33 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if(matrix === undefined || matrix.length === 0) {
+
+      return [];
+
+    } else {
+
+      let mamatrixNumReverse = [];
+      
+      for(let i = 0; i < matrix.length; i++) {
+          if(i === 0) {
+              mamatrixNumReverse.push(matrix[i]); 
+          } else if (i % 2 != 0) {
+              mamatrixNumReverse.push(matrix[i].reverse());
+          } else {
+              mamatrixNumReverse.push(matrix[i]);
+          }
+      }
+
+      let matrixStr = String(mamatrixNumReverse).split(',');
+      let matrixNum =[];
+
+      for(let i = 0; i < matrixStr.length; i++) {
+          matrixNum.push(+matrixStr[i]);
+      }
+
+      return matrixNum;
+    }
+
 }
